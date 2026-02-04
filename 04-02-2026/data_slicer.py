df4044002850991cb3288c253270b6ff310ba2b5
M-Supritha <suprithamahabala13@gmail.com>
2026-02-04 11:09:37 +0530

    Updated code

---
+++ 04-02-2026/data_slicer.py
@@ -0,0 +1,5 @@
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print("First Reading:",temperatures[0])
+print("last Reading:",temperatures[-1])
+print("Afternoon peak:",temperatures[3:6])
+print("Last 3 Hours Reading:",temperatures[-3:])
