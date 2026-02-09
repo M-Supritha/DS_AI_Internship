f46cfeed068dbd7320a42a894f70a1c4302b47ba
M-Supritha <suprithamahabala13@gmail.com>
2026-02-09 10:41:13 +0530

    Initial Commit

---
+++ 07-02-2026/personal_logger.py
@@ -0,0 +1,8 @@
+
+name = input("Enter your name: ")
+goal = input("Enter your daily goal: ")
+
+with open("journal.txt", "a") as file:
+    file.write(f"Name: {name}, Daily Goal: {goal}\n")
+
+print("Your goal has been saved successfully!")
